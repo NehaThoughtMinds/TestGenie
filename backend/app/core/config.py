305e36e5c43a743a13c3c3d9599dc36ea01cab59
surya2001-maker <surya@thoughtminds.io>
2026-02-17 19:33:34 +0530

    feat:added api for upload and generating the testcases

---
+++ backend/app/core/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    APP_NAME: str = "TestForge AI"
+    OPEN_API_KEY: str
+    ALLOWED_ORIGINS: list[str] = ["*"]
+    MAX_TOKENS: int = 4096
+    MODEL: str = "gpt-4o-mini"
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
